71af69b79f0da22c174e85f7774c54b633474a59
Jayson Armbruster <Jbru95@outlook.com>
2023-12-02 11:46:33 -0500

    Finished Day 1

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,1 +1,34 @@
-#First commit comment
+
+total = 0
+
+fp = open('day1Changed.txt', 'r')
+
+for line in fp.readlines():
+    strippedLine = line.strip()
+    print(strippedLine)
+    justNums = []
+
+    for char in strippedLine: 
+        if char.isdigit(): 
+            justNums.append(char)
+
+    firstAndLastString = justNums[0] + justNums[-1]
+
+    print(firstAndLastString)
+
+    total += int(firstAndLastString)
+
+print(total)
+fp.close()
+
+# # 'one two three four five six seven eight nine'
+# # oneight
+# # threeight
+# # nineight
+# # fiveight
+# # sevenine
+# # eightwo
+# # eighthree
+
+
+
